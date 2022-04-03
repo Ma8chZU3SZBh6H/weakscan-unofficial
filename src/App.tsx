6d33120ee2d04a3e20cdae44a9b11fc1302baaf3
skindervik <38836685+skindervik@users.noreply.github.com>
2022-04-03 20:55:59 +0300

    fancy particles

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,19 @@ import Routes from './Routes';
 import Cube from './Pages/Home/Components/Cube';
 import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
+import Particles from 'react-tsparticles';
+import { Container, Engine } from 'tsparticles';
 
 export default function App() {
+  const particlesInit = async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = async (container: Container) => {
+    console.log(container);
+  };
   return (
     <div className="bg-night-blue  min-h-screen text-sky-blue  grid grid-rows-auto-1fr xl:grid-cols-auto-1fr xl:grid-rows-1 p-10 gap-10 overflow-hidden">
       <div className="grid grid-rows-auto-1fr gap-10 ">
@@ -51,6 +62,81 @@ export default function App() {
         </div>
         <Cube className="w-[50vh] h-[50vh] animate-cube-spin" />
         <Cube className="w-[60vh] h-[60vh] animate-cube-spin2" />
+
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          className={'opacity-25'}
+          options={{
+            fpsLimit: 60,
+            interactivity: {
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 50,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: '#ffffff',
+              },
+              links: {
+                color: '#ffffff',
+                distance: 200,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: 'none',
+                enable: true,
+                outMode: 'bounce',
+                random: false,
+                speed: 1,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: 'circle',
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
       </div>
     </div>
   );
