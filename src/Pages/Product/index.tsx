36ef4b2331e9ce530eb5f549ba134f26a4639583
skindervik <38836685+skindervik@users.noreply.github.com>
2022-04-03 20:01:42 +0300

    price send user to contact page

--- src/Pages/Product/index.tsx
+++ src/Pages/Product/index.tsx
@@ -1,15 +1,19 @@
 import Lock from '../../Images/Lock';
 import { Product as PRODUCT } from './product.types';
+import { Link } from 'wouter';
 
-export default function Product({ price, title, description }: PRODUCT) {
+export default function Product({ price, title, description, path }: PRODUCT) {
   return (
     <div className="flex flex-col items-center justify-center gap-5 text-center font-medium">
       <Lock className="text-day-blue w-28 h-28" />
       <h1 className="md:text-64px text-48px">{title} </h1>
       <p className="max-w-[497px]">{description}</p>
-      <div className="bg-light-blue text-night-blue text-64px px-4 leading-[70px] pb-1 mt-4">
+      <Link
+        href="/contact"
+        className="cursor-pointer bg-light-blue text-night-blue text-64px px-4 leading-[70px] pb-1 mt-4"
+      >
         {price}
-      </div>
+      </Link>
     </div>
   );
 }
