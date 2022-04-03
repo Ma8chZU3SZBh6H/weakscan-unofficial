cec74b4ed02c8df45e06611d49f8e8f5c7d90669
skindervik <38836685+skindervik@users.noreply.github.com>
2022-04-03 19:58:30 +0300

    fake email send

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,6 +1,6 @@
 import Input from './components/Input';
 import TextArea from './components/TextArea';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { classValidatorResolver } from '@hookform/resolvers/class-validator';
 import { Email } from './contact.zod';
@@ -14,51 +14,61 @@ export default function Contact() {
     formState: { errors },
   } = useForm({ resolver });
 
-  const onSubmit = (data: any) => console.log(data, errors);
+  const onSubmit = (data: any) => setSent(true);
 
-  useEffect(() => {
-    console.log(errors['firstName']?.message);
-  }, [errors]);
+  const [sent, setSent] = useState(false);
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col gap-10 max-w-md"
-    >
-      <h1 className="text-center text-48px md:text-64px">Contact us.</h1>
-      <div className="flex gap-10 md:flex-row flex-col">
-        <Input
-          name="firstName"
-          title="First name"
-          onChange={register}
-          errors={errors}
-        />
-        <Input
-          name="lastName"
-          title="Last name"
-          onChange={register}
-          errors={errors}
-        />
-      </div>
-      <Input
-        name="email"
-        title="Email"
-        type="email"
-        onChange={register}
-        errors={errors}
-      />
-      <TextArea
-        title="Message"
-        name="message"
-        onChange={register}
-        errors={errors}
-      />
-      <button
-        type="submit"
-        className="bg-light-blue text-night-blue text-64px px-4 leading-[70px] pb-1 mt-4 mx-auto"
+  if (!sent) {
+    return (
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-10 max-w-md"
       >
-        SEND
-      </button>
-    </form>
-  );
+        <div>
+          <h1 className="text-center text-48px md:text-64px">Contact us.</h1>
+          <h2 className="text-center">example@page.com</h2>
+        </div>
+        <div className="flex gap-10 md:flex-row flex-col">
+          <Input
+            name="firstName"
+            title="First name"
+            onChange={register}
+            errors={errors}
+          />
+          <Input
+            name="lastName"
+            title="Last name"
+            onChange={register}
+            errors={errors}
+          />
+        </div>
+        <Input
+          name="email"
+          title="Email"
+          type="email"
+          onChange={register}
+          errors={errors}
+        />
+        <TextArea
+          title="Message"
+          name="message"
+          onChange={register}
+          errors={errors}
+        />
+        <button
+          type="submit"
+          className="bg-light-blue text-night-blue text-64px px-4 leading-[70px] pb-1 mt-4 mx-auto"
+        >
+          SEND
+        </button>
+      </form>
+    );
+  } else {
+    return (
+      <h1 className="text-48px max-w-md text-center">
+        <strong>Email was "sent" to example@page.com</strong> - not actually,
+        this is an example page
+      </h1>
+    );
+  }
 }
